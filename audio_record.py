594a71930b3e7d1d6c6cb1a9cb5c25752e5a3949
nazifatanzim <44646521+nazifatanzim@users.noreply.github.com>
2021-12-06 16:10:28 -0500

    Update audio_record.py
    Add documentation
    Use relative path

--- audio_record.py
+++ audio_record.py
@@ -1,16 +1,23 @@
 import pyaudio
 import wave
 
-
 CHUNK = 1024
 FORMAT, CHANNELS = pyaudio.paInt16, 1
 RATE = 16000
 RECORD_SECONDS = 2
-# WAVE_OUTPUT_FILENAME = "C:/Users/anwar_tmk/Documents/Carleton/4th Year/4th year project/SpeechBot/input/audio/Christopher/Christopher53.wav"
 
-num = 90
+# Change the following 3 variables (num, word, iterations) according to the intended filename and path
+# num will be used as part of the filename and idicates the iteration of the word that is being recorded.
+# word is used for the filename and file path. 
+# for example, if you are recording the word "hello" for the first time, num = 1, word = "hello". this will be stored in a folder called
+# "hello" and the filename will be "hello1.wav"
+# iterations indicates the number of recordings to be made for the specific word in one go.
+# if num=1, word="hello", and iterations=5, then you will be prompted to record the word 5 times in 1 session
+# the expected outputs in folder "hello" will be hello1.wav, hello2.wav, hello3.wav, hello4.wav, hello5.wav
+num = 105
 word = "Nicolas"
-for x in range(3):
+iterations = 2
+for x in range(iterations):
 	p = pyaudio.PyAudio()
 
 	stream = p.open(format=FORMAT,
@@ -35,7 +42,7 @@ for x in range(3):
 	stream.close()
 	p.terminate()
 
-	WAVE_OUTPUT_FILENAME = "C:/Users/anwar_tmk/Documents/Carleton/4th Year/4th year project/SpeechBot/input/audio/{0}/{1}{2}.wav".format(word, word, num)
+	WAVE_OUTPUT_FILENAME = "./input/audio/{0}/{1}{2}.wav".format(word, word, num)
 	wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
 	wf.setnchannels(CHANNELS)
 	wf.setsampwidth(p.get_sample_size(FORMAT))
