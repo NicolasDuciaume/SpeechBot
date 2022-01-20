7aa2c109836f0bf3b91092a959d30797e8dc944f
NicolasDuciaume <duciaumenicolas@gmail.com>
2022-01-20 15:11:36 -0500

    New code

--- Prototype_Microphone.py
+++ Prototype_Microphone.py
@@ -1,91 +1,70 @@
-import pyaudio
-import tensorflow as tf
+from ctypes import sizeof
+from keras.models import load_model
 import numpy as np
-import struct
-import glob
-from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.layers import LSTM, Input, Dense, BatchNormalization, Conv2D, MaxPooling2D, Dropout, Flatten, TimeDistributed
-from tensorflow.keras.layers.experimental import preprocessing
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras import layers
-import matplotlib.pyplot as plt
-import wave
+model=load_model('D:/speech2/best_model.hdf5')
 
-words=['Nicolas', 'Nazifa', 'Christopher', 'Mohammad']
-block_length = 0.050#->500ms
-voice_max_length = int(1/block_length)#->2s
-wordToId, idToWord = {}, {}
+all_label = ["yes", "no", "up", "down","left", "right", "on", "off", "stop", "go"]
 
-for i, word in enumerate(words):
-    wordToId[word], idToWord[i] = i, word
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y=le.fit_transform(all_label)
+classes= list(le.classes_)
 
-def audioToTensor(filepath):
-    audio_binary = tf.io.read_file(filepath)
-    audio, audioSR = tf.audio.decode_wav(audio_binary)
-    audioSR = tf.get_static_value(audioSR)
-    audio = tf.squeeze(audio, axis=-1)
-    audio_lenght = int(audioSR * block_length)#->16000*0.5=8000
-    frame_step = int(audioSR * 0.008)#16000*0.008=128
-    if len(audio)<audio_lenght*voice_max_length:
-        audio = tf.concat([np.zeros([audio_lenght*voice_max_length-len(audio)]), audio], 0)
+
+def predict(audio):
+    prob=model.predict(audio.reshape(1,8000,1))
+    index=np.argmax(prob[0])
+    return classes[index]
+
+import sounddevice as sd
+import soundfile as sf
+import librosa
+import os
+import IPython.display as ipd
+import time
+
+samplerate = 16000  
+duration = 1 # seconds
+filename = 'D:/speech2/test'
+
+def pad_audio(data, fs, T):
+    # Calculate target number of samples
+    N_tar = fs
+    # Calculate number of zero samples to append
+    shape = data.shape
+    # Create the target shape    
+    N_pad = N_tar - shape[0]
+    #print("Padding with %s seconds of silence" % str(N_pad/fs) )
+    shape = (N_pad,) + shape[1:]
+    # Stack only if there is something to append    
+    if shape[0] > 0:                
+        if len(shape) > 1:
+            return np.vstack((np.zeros(shape),
+                              data))
+        else:
+            return np.hstack((np.zeros(shape),
+                              data))
     else:
-        audio = audio[-(audio_lenght*voice_max_length):]
-    spectrogram = tf.signal.stft(audio, frame_length=1024, frame_step=frame_step)
-    spectrogram = (tf.math.log(tf.abs(tf.math.real(spectrogram)))/tf.math.log(tf.constant(10, dtype=tf.float32))*20)-60
-    spectrogram = tf.where(tf.math.is_nan(spectrogram), tf.zeros_like(spectrogram), spectrogram)
-    spectrogram = tf.where(tf.math.is_inf(spectrogram), tf.zeros_like(spectrogram), spectrogram)
-    voice_length, voice = 0, []
-    nb_part = len(audio)//audio_lenght
-    part_length = len(spectrogram)//nb_part
-    partsCount = len(range(0, len(spectrogram)-part_length, int(part_length/2)))
-    parts = np.zeros((partsCount, part_length, 513))
-    for i, p in enumerate(range(0, len(spectrogram)-part_length, int(part_length/2))):
-        part = spectrogram[p:p+part_length]
-        parts[i] = part
-    return parts
+        return data
 
-model = tf.keras.models.load_model('D:/best_model/model_word')
+#print("start")
+for x in range(6):
+    print("start")
+    mydata = sd.rec(int(samplerate * duration), samplerate=samplerate,
+        channels=1, blocking=True)
+    mydata = pad_audio(mydata,samplerate, 0.2)
+    sd.wait()
+    sf.write(filename+str(x)+".wav", mydata, samplerate)
+    print("end")
+    #time.sleep(0)
 
+#print("end")
 
-CHUNK = 1024
-FORMAT, CHANNELS = pyaudio.paInt16, 1
-RATE = 16000
-RECORD_SECONDS = 2
-WAVE_OUTPUT_FILENAME = "output.wav"
+os.listdir('D:/speech2')
+filepath='D:/speech2'
 
-p = pyaudio.PyAudio()
-
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
-
-
-print("* recording")
-
-frames = []
-
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
-
-print("* done recording")
-
-stream.stop_stream()
-stream.close()
-p.terminate()
-
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
-
-test_audio = audioToTensor(WAVE_OUTPUT_FILENAME)
-result = model.predict(np.array([test_audio]))
-max = np.argmax(result)
-print("decoded_sentence: ", result, max, idToWord[max])
-
-
+for y in range(6):
+    samples, sample_rate = librosa.load(filepath + '/' + 'test' + str(y) + '.wav', sr = 16000)
+    samples = librosa.resample(samples, sample_rate, 8000)
+    ipd.Audio(samples,rate=8000)
+    print(predict(samples))
--- Training_model.py
+++ Training_model.py
@@ -1,98 +1,160 @@
-import numpy as np
-import glob
-import tensorflow as tf
-from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.layers import LSTM, Input, Dense, BatchNormalization, Conv2D, MaxPooling2D, Dropout, Flatten, TimeDistributed
-from tensorflow.keras.layers.experimental import preprocessing
-from tensorflow.keras.optimizers import SGD
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras import layers
+import os
+import librosa
+import IPython.display as ipd
 import matplotlib.pyplot as plt
-words=['Nicolas', 'Nazifa', 'Christopher']
-block_length = 0.050#->500ms
-voice_max_length = int(1/block_length)#->2s
-print("voice_max_length:", voice_max_length)
-def audioToTensor(filepath):
-    audio_binary = tf.io.read_file(filepath)
-    audio, audioSR = tf.audio.decode_wav(audio_binary)
-    audioSR = tf.get_static_value(audioSR)
-    audio = tf.squeeze(audio, axis=-1)
-    audio_lenght = int(audioSR * block_length)#->16000*0.5=8000
-    frame_step = int(audioSR * 0.008)#16000*0.008=128
-    if len(audio)<audio_lenght*voice_max_length:
-        audio = tf.concat([np.zeros([audio_lenght*voice_max_length-len(audio)]), audio], 0)
-    else:
-        audio = audio[-(audio_lenght*voice_max_length):]
-    spectrogram = tf.signal.stft(audio, frame_length=1024, frame_step=frame_step)
-    spectrogram = (tf.math.log(tf.abs(tf.math.real(spectrogram)))/tf.math.log(tf.constant(10, dtype=tf.float32))*20)-60
-    spectrogram = tf.where(tf.math.is_nan(spectrogram), tf.zeros_like(spectrogram), spectrogram)
-    spectrogram = tf.where(tf.math.is_inf(spectrogram), tf.zeros_like(spectrogram), spectrogram)
-    voice_length, voice = 0, []
-    nb_part = len(audio)//audio_lenght
-    part_length = len(spectrogram)//nb_part
-    partsCount = len(range(0, len(spectrogram)-part_length, int(part_length/2)))
-    parts = np.zeros((partsCount, part_length, 513))
-    for i, p in enumerate(range(0, len(spectrogram)-part_length, int(part_length/2))):
-        part = spectrogram[p:p+part_length]
-        parts[i] = part
-    return parts
-max_data = 100
-wordToId, idToWord = {}, {}
-testParts = audioToTensor('D:/Final_Project/SpeechBot/input/audio/Nicolas/Nicolas3.wav')
-print(testParts.shape)
-X_audio, Y_word = np.zeros((max_data*3, testParts.shape[0], testParts.shape[1], testParts.shape[2])), np.zeros((max_data*3, 3))
+import numpy as np
+from scipy.io import wavfile
+import warnings
 
-files = {}
-for i, word in enumerate(words):
-    wordToId[word], idToWord[i] = i, word
-    files[word] = glob.glob('D:/Final_Project/SpeechBot/input/audio/'+word+'/*.wav')
-for nb in range(0, max_data):
-    for i, word in enumerate(words):
-        audio = audioToTensor(files[word][nb])
-        X_audio[len(files)*nb + i] = audio
-        Y_word[len(files)*nb + i] = np.array(to_categorical([i], num_classes=len(words))[0])
+warnings.filterwarnings("ignore")
+os.listdir('D:/speech2/input/')
+train_audio_path = 'D:/speech2/input/tensorflow-speech-recognition-challenge/train/audio/'
+samples, sample_rate = librosa.load(train_audio_path+'yes/0a7c2a8d_nohash_0.wav', sr = 16000)
+fig = plt.figure(figsize=(14, 8))
+ax1 = fig.add_subplot(211)
+ax1.set_title('Raw wave of ' + 'D:/speech2/input/train/audio/yes/0a7c2a8d_nohash_0.wav')
+ax1.set_xlabel('time')
+ax1.set_ylabel('Amplitude')
+ax1.plot(np.linspace(0, sample_rate/len(samples), sample_rate), samples)
 
-X_audio_test, Y_word_test = X_audio[int(len(X_audio)*0.8):], Y_word[int(len(Y_word)*0.8):]
-X_audio, Y_word = X_audio[:int(len(X_audio)*0.8)], Y_word[:int(len(Y_word)*0.8)]
-print("X_audio.shape: ", X_audio.shape)
-print("Y_word.shape: ", Y_word.shape)
-print("X_audio_test.shape: ", X_audio_test.shape)
-print("Y_word_test.shape: ", Y_word_test.shape)
-latent_dim=32
-encoder_inputs = Input(shape=(testParts.shape[0], None, None, 1))
-preprocessing = TimeDistributed(preprocessing.Resizing(6, 129))(encoder_inputs)
-normalization = TimeDistributed(BatchNormalization())(preprocessing)
-conv2d = TimeDistributed(Conv2D(34, 3, activation='relu'))(normalization)
-conv2d = TimeDistributed(Conv2D(64, 3, activation='relu'))(conv2d)
-maxpool = TimeDistributed(MaxPooling2D())(conv2d)
-dropout = TimeDistributed(Dropout(0.25))(maxpool)
-flatten = TimeDistributed(Flatten())(dropout)
-encoder_lstm = LSTM(units=latent_dim)(flatten)
-decoder_dense = Dense(len(words), activation='relu')(encoder_lstm)
-model = Model(encoder_inputs, decoder_dense)
-opt = SGD(lr=0.005)
-model.compile(loss = "mse", optimizer = opt, metrics=['acc'])
-##model.compile(optimizer=tf.keras.optimizers.Adam(), loss='categorical_crossentropy', metrics=['acc'])
-model.summary(line_length=200)
-tf.keras.utils.plot_model(model, to_file='D:/Final_Project/SpeechBot/model_word.png', show_shapes=True)
-batch_size = 12
-epochs = 100
-history=model.fit(X_audio, Y_word, shuffle=True, batch_size=batch_size, epochs=epochs, steps_per_epoch=len(X_audio)//batch_size, validation_data=(X_audio_test, Y_word_test)) ##shuffle was false
-model.save_weights('D:/Final_Project/SpeechBot/model_word.h5')
-model.save("D:/Final_Project/SpeechBot/model_word")
-metrics = history.history
-plt.plot(history.epoch, metrics['loss'], metrics['acc'])
-plt.legend(['loss', 'acc'])
-plt.savefig("learning-word.png")
+ipd.Audio(samples, rate=sample_rate)
+
+print(sample_rate)
+
+samples = librosa.resample(samples, sample_rate, 8000)
+ipd.Audio(samples, rate=8000)
+
+labels=os.listdir(train_audio_path)
+
+#find count of each label and plot bar graph
+no_of_recordings=[]
+for label in labels:
+    waves = [f for f in os.listdir(train_audio_path + '/'+ label) if f.endswith('.wav')]
+    no_of_recordings.append(len(waves))
+    
+#plot
+plt.figure(figsize=(30,5))
+index = np.arange(len(labels))
+plt.bar(index, no_of_recordings)
+plt.xlabel('Commands', fontsize=12)
+plt.ylabel('No of recordings', fontsize=12)
+plt.xticks(index, labels, fontsize=15, rotation=60)
+plt.title('No. of recordings for each command')
 plt.show()
-plt.close()
-score = model.evaluate(X_audio_test, Y_word_test, verbose = 0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
-print("Test voice recognition")
-for test_path, test_string in [('D:/Final_Project/SpeechBot/input/audio/Nazifa/Nazifa2.wav', 'Nazifa'), ('D:/Final_Project/SpeechBot/input/audio/Christopher/Christopher4.wav', 'Christopher')]:
-    print("test_string: ", test_string)
-    test_audio = audioToTensor(test_path)
-    result = model.predict(np.array([test_audio]))
-    max = np.argmax(result)
-    print("decoded_sentence: ", result, max, idToWord[max])
+
+
+labels=["yes", "no", "up", "down","left", "right", "on", "off", "stop", "go"]
+#"left", "right", "on", "off", "stop", "go"
+
+duration_of_recordings=[]
+for label in labels:
+    waves = [f for f in os.listdir(train_audio_path + '/'+ label) if f.endswith('.wav')]
+    for wav in waves:
+        sample_rate, samples = wavfile.read(train_audio_path + '/' + label + '/' + wav)
+        duration_of_recordings.append(float(len(samples)/sample_rate))
+    
+plt.hist(np.array(duration_of_recordings))
+
+train_audio_path = 'D:/speech2/input/tensorflow-speech-recognition-challenge/train/audio/'
+
+all_wave = []
+all_label = []
+for label in labels:
+    print(label)
+    waves = [f for f in os.listdir(train_audio_path + '/'+ label) if f.endswith('.wav')]
+    for wav in waves:
+        samples, sample_rate = librosa.load(train_audio_path + '/' + label + '/' + wav, sr = 16000)
+        samples = librosa.resample(samples, sample_rate, 8000)
+        if(len(samples)== 8000) : 
+            all_wave.append(samples)
+            all_label.append(label)
+            
+#print("what")
+            
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y=le.fit_transform(all_label)
+classes= list(le.classes_)
+
+from keras.utils import np_utils
+y=np_utils.to_categorical(y, num_classes=len(labels))
+
+all_wave = np.array(all_wave).reshape(-1,8000,1)
+
+from sklearn.model_selection import train_test_split
+x_tr, x_val, y_tr, y_val = train_test_split(np.array(all_wave),np.array(y),stratify=y,test_size = 0.2,random_state=777,shuffle=True)
+
+from keras.layers import Dense, Dropout, Flatten, Conv1D, Input, MaxPooling1D
+from keras.models import Model
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras import backend as K
+K.clear_session()
+
+inputs = Input(shape=(8000,1))
+
+#First Conv1D layer
+conv = Conv1D(8,13, padding='valid', activation='relu', strides=1)(inputs)
+conv = MaxPooling1D(3)(conv)
+conv = Dropout(0.3)(conv)
+
+#Second Conv1D layer
+conv = Conv1D(16, 11, padding='valid', activation='relu', strides=1)(conv)
+conv = MaxPooling1D(3)(conv)
+conv = Dropout(0.3)(conv)
+
+#Third Conv1D layer
+conv = Conv1D(32, 9, padding='valid', activation='relu', strides=1)(conv)
+conv = MaxPooling1D(3)(conv)
+conv = Dropout(0.3)(conv)
+
+#Fourth Conv1D layer
+conv = Conv1D(64, 7, padding='valid', activation='relu', strides=1)(conv)
+conv = MaxPooling1D(3)(conv)
+conv = Dropout(0.3)(conv)
+
+#Flatten layer
+conv = Flatten()(conv)
+
+#Dense Layer 1
+conv = Dense(256, activation='relu')(conv)
+conv = Dropout(0.3)(conv)
+
+#Dense Layer 2
+conv = Dense(128, activation='relu')(conv)
+conv = Dropout(0.3)(conv)
+
+outputs = Dense(len(labels), activation='softmax')(conv)
+
+model = Model(inputs, outputs)
+model.summary()
+
+model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=10, min_delta=0.0001) 
+mc = ModelCheckpoint('D:/speech2/best_model.hdf5', monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
+
+history=model.fit(x_tr, y_tr ,epochs=50, callbacks=[es,mc], batch_size=32, validation_data=(x_val,y_val))
+
+
+from matplotlib import pyplot
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+pyplot.legend()
+pyplot.show()
+
+from keras.models import load_model
+model=load_model('D:/speech2/best_model.hdf5')
+
+def predict(audio):
+    prob=model.predict(audio.reshape(1,8000,1))
+    index=np.argmax(prob[0])
+    return classes[index]
+
+import random
+index=random.randint(0,len(x_val)-1)
+samples=x_val[index].ravel()
+print("Audio:",classes[np.argmax(y_val[index])])
+ipd.Audio(samples, rate=8000)
+
+print("Text:",predict(samples))
+
