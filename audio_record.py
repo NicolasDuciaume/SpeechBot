8594c731b9e371fe6baba8bdebb92d27d553f301
Chris <chrisdsilva499@gmail.com>
2022-02-24 22:32:18 -0500

    Added more voice recordings

--- audio_record.py
+++ audio_record.py
@@ -7,7 +7,7 @@ import time
 
 samplerate = 16000  
 duration = 1.5 # seconds
-filename = './input/audio/yes/yes'
+filename = './input/audio/worth/worth'
 #filename = './input/rerecord/yes'
 
 for x in range(50):
